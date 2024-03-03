e230b4aef28fbb99dcb9d75df0c5c79d50fce9f2
richwill28 <e0550368@u.nus.edu>
2024-03-03 16:59:36 +0800

    Initial engine implementation with relevance optimization

---
+++ src/relevance/hippodrome.cpp
@@ -0,0 +1,27 @@
+#include "engine.h"
+#include <chrono>
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    std::cerr << "Usage: ./hippodrome <path to map> <path to grammar>\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::string map_path{argv[1]};
+  std::string grammar_path{argv[2]};
+
+  auto time_begin = std::chrono::high_resolution_clock::now();
+
+  Engine engine{};
+  engine.analyze(map_path, grammar_path);
+
+  auto time_end = std::chrono::high_resolution_clock::now();
+
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+                      time_end - time_begin)
+                      .count();
+
+  std::cout << "Time elapsed = " << duration << " msec\n";
+}
